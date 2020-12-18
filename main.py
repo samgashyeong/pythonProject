c793582f2cfb8ed3be09ced891a67db2e57fbd9d
samgashyeong <samgashyeong@naver.com>
2020-12-18 13:57:10 +0900

    그래프

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from matplotlib import pylab as pit
 import numpy as np
-import math
 
 a = int(input('개수 입력'))
 
-x = np.arange(1, a)
+x = np.arange(1, a+1)
 y = np.log(x)
 
 pit.plot(x, y, 'or')
+pit.annotate('Maximum number of data', xy=(a, np.log(a)), xytext=(0, 0), arrowprops={'color': 'blue'})
 pit.xlabel('Number of times to perform')
 pit.ylabel('Number of data')
 pit.title('a dichotomous graph')
